1006445ea5e6c69a5423116a672de9ce4250a789
Tobias <tobi@pc.fritz.box>
2020-03-21 16:30:23 +0100

    set jackson jsonmapper to only use explicit json properties and added delete methods

--- src/main/kotlin/bedbrains/homesweethomeserver/rest/Controller.kt
+++ src/main/kotlin/bedbrains/homesweethomeserver/rest/Controller.kt
@@ -1,13 +1,12 @@
 package bedbrains.homesweethomeserver.rest
 
-import bedbrains.shared.datatypes.temperature.celsius
 import bedbrains.shared.datatypes.devices.Device
 import bedbrains.shared.datatypes.devices.Heating
 import bedbrains.shared.datatypes.devices.Light
 import bedbrains.shared.datatypes.rules.*
+import bedbrains.shared.datatypes.temperature.celsius
 import bedbrains.shared.datatypes.upsert
 import org.springframework.web.bind.annotation.*
-
 
 @RestController
 class Controller {
@@ -20,29 +19,57 @@ class Controller {
             WeeklyRule("10000", "Normal").apply { timeSpans.add(WeeklyTimeSpan(WeeklyTime.now, WeeklyTime.MAX)) },
             WeeklyRule("10001", "Ferien")
     )
+    val values = mutableListOf<RuleValue>(
+            RuleValue("10002", "Default", 22.celsius, true),
+            RuleValue("10003", "Winter", 23.celsius, true)
+    )
 
     @GetMapping("/devices")
-    fun devices(): List<Device.JSON> = devices.map { it.toJSON() }
+    fun devices() = devices
 
     @GetMapping("/device")
-    fun device(@RequestParam(name = "uid") uid: String): Device.JSON? = devices.find { it.uid == uid }?.toJSON()
+    fun device(@RequestParam(name = "uid") uid: String) = devices.find { it.uid == uid }
 
-    @PostMapping("device")
-    fun device(@RequestBody json: Device.JSON) {
-        val device = json.toDevice()
+    @PostMapping("/device")
+    fun postDevice(@RequestBody device: Device) {
         devices.upsert(device) { it.uid == device.uid }
     }
 
+    @DeleteMapping("/device")
+    fun deleteDevice(@RequestParam(name = "uid") uid: String) {
+        devices.removeIf { it.uid == uid }
+    }
+
     @GetMapping("/rules")
-    fun rules(): List<Rule.JSON> = rules.map { it.toJSON() }
+    fun rules() = rules
 
     @GetMapping("/rule")
-    fun rule(@RequestParam(name = "uid") uid: String): Rule.JSON? = rules.find { it.uid == uid }?.toJSON()
+    fun rule(@RequestParam(name = "uid") uid: String) = rules.find { it.uid == uid }
 
-    @PostMapping("rule")
-    fun device(@RequestBody json: Rule.JSON) {
-        val rule = json.toRule()
+    @PostMapping("/rule")
+    fun postrule(@RequestBody rule: Rule) {
         rules.upsert(rule) { it.uid == rule.uid }
+    }
+
+    @DeleteMapping("/rule")
+    fun deleteRule(@RequestParam(name = "uid") uid: String) {
+        rules.removeIf { it.uid == uid }
+    }
+
+    @GetMapping("/values")
+    fun values() = values
+
+    @GetMapping("/value")
+    fun value(@RequestParam(name = "uid") uid: String) = values.find { it.uid == uid }
+
+    @PostMapping("/value")
+    fun postValue(@RequestBody value: RuleValue) {
+        values.upsert(value) { it.uid == value.uid }
+    }
+
+    @DeleteMapping("/value")
+    fun deleteValue(@RequestParam(name = "uid") uid: String) {
+        values.removeIf { it.uid == uid }
     }
 
 }
