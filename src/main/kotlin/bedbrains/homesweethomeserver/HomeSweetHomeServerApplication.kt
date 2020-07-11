3d825a2b26042e6933b3cb563768fae7a603441d
einHollander <gaert.mennchen@gmail.com>
2020-07-11 23:47:08 +0200

    Implemented a first MQtt Test Using HiveMQ MQtt Clienet.

--- src/main/kotlin/bedbrains/homesweethomeserver/HomeSweetHomeServerApplication.kt
+++ src/main/kotlin/bedbrains/homesweethomeserver/HomeSweetHomeServerApplication.kt
@@ -1,5 +1,10 @@
 package bedbrains.homesweethomeserver
 
+import bedbrains.platform.UIDProvider
+import com.hivemq.client.mqtt.datatypes.MqttQos
+import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client
+import org.apache.tomcat.jni.Socket.send
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +13,27 @@ class HomeSweetHomeServerApplication
 
 fun main(args: Array<String>) {
     runApplication<HomeSweetHomeServerApplication>(*args)
+
+    val clientIdentifier: String = UIDProvider.newUID
+
+    val client: Mqtt5AsyncClient = Mqtt5Client.builder()
+            .identifier(clientIdentifier)
+            .serverHost("localhost")
+            .serverPort(1883)
+            .buildAsync()
+
+    client.connect()
+
+    client.subscribeWith()
+            .topicFilter("test")
+            .qos(MqttQos.EXACTLY_ONCE)
+            .callback {
+                println(it.contentType)
+                println(it)
+            }
+            .send()
+
+
+    //ToDo: Remove print statement
+    println("setup done ;)")
 }
